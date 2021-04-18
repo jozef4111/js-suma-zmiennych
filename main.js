d29bbb4b82589d0d49a6f507d9dd7ce342502f68
jozef4111 <jozeff4.522@gmail.com>
2021-04-18 21:42:55 +0200

    suma zmiennych

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstNumber = '22';
+
+let secondNumber = '77';
+
+function math(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+ console.log(math(22, 35));
